1a83c1a54ab92b0b6fbcf700afc549db9da00a34
Liu Zhongyu <lzy19980525@yeah.net>
2019-04-30 23:05:42 +0800

    构建 Servlet 的框架。TODO：完成内容的填充

---
+++ src/xiuxiuxiu/servelt/RepairingReservationServlet.java
@@ -0,0 +1,68 @@
+package xiuxiuxiu.servelt;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import xiuxiuxiu.util.Page;
+
+/**
+ * RepairingReservationServlet
+ * 
+ * 用于处理用户端维修预约页面的后台请求
+ * 
+ * @author Lenovo
+ *
+ */
+public class RepairingReservationServlet extends BaseServlet {
+
+    /* (non-Javadoc)
+     * @see xiuxiuxiu.servelt.BaseServlet#add(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, xiuxiuxiu.util.Page)
+     */
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see xiuxiuxiu.servelt.BaseServlet#delete(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, xiuxiuxiu.util.Page)
+     */
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see xiuxiuxiu.servelt.BaseServlet#edit(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, xiuxiuxiu.util.Page)
+     */
+    @Override
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see xiuxiuxiu.servelt.BaseServlet#update(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, xiuxiuxiu.util.Page)
+     */
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see xiuxiuxiu.servelt.BaseServlet#list(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, xiuxiuxiu.util.Page)
+     */
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+        // TODO Auto-generated method stub
+        super.service(request, response);
+    }
+}
