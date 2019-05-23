9a8255a20d9d97905a8e48080f0cb5327ba3ee3d
huangquanhuan <46898209+huangquanhuan@users.noreply.github.com>
2019-05-23 22:45:08 +0800

    提交预约维修类型的表单修改，尝试解决上次上传失败导致的冲突

--- src/main/java/com/xiuxiuxiu/model/RepairType.java
+++ src/main/java/com/xiuxiuxiu/model/RepairType.java
@@ -1,19 +1,27 @@
 package com.xiuxiuxiu.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 //待修改
 /**
  * RepairType - 维修类型类
  * 
- * @author 向鹏
+ * @author 黄权焕
  * @date 2019-5-23
  */
+@Entity
+@Table(name = "RepairType")
 public class RepairType {
 
 	/** 维修问题id */
+	@Id
+	@GeneratedValue
 	private int id;
 	/** 维修类型: */
+	@Column(nullable = false, unique = true)
 	private String Type;
-	/** 详细分类 */
-	private String classify;
-
-	
 }
