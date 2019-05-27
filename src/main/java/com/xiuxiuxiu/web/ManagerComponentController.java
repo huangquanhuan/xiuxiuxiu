db176ef7394bb44de75a4f4140615a50c21c51d3
huige777 <398315418@qq.com>
2019-05-28 02:28:05 +0800

    1

--- src/main/java/com/xiuxiuxiu/web/ManagerComponentController.java
+++ src/main/java/com/xiuxiuxiu/web/ManagerComponentController.java
@@ -21,7 +21,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-public class ManagerArticleController {
+public class ManagerComponentController {
 
 	@Resource
 	ArticleService articleService;
